24cf82cabeb816bbcd810f4f169c68f7bec69f16
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2020-08-13 13:16:51 +0200

    Ajout d'un module de comunication avec la starcitizen API

---
+++ src/interfaces/restAPI/scAPI.js
@@ -0,0 +1,18 @@
+const get = require('../../util/xhrRequest')
+const log = require("../../util/logger");
+
+module.exports = {
+    async getUser(handle) {
+        try {
+            let apiJSON = await get(`https://api.starcitizen-api.com/${process.env.APIKEY_SC}/v1/live/user/${handle}`)
+            apiJSON = JSON.parse(apiJSON)
+            if (apiJSON.data.profile){
+                return apiJSON.data
+            }else {
+                return log.warn('Le handle n\'existe pas')
+            }
+        } catch (e) {
+            return e
+        }
+    }
+}
