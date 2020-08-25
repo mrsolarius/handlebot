9c5de56b425f1f0a30872a345549898fca6e17a2
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2020-08-25 21:19:14 +0200

    support des lang par la commande ship

--- src/commands/ship/displayShip.js
+++ src/commands/ship/displayShip.js
@@ -7,18 +7,18 @@ const capitalize = (s) => {
     return s.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
-const componentStringBuilder = (components) =>{
+const componentStringBuilder = (components,lang) =>{
     let str=""
     for (const component of components) {
         str += "```autohotkey" +
             "\n#" +component.name+"#"
-            "\nFabriquant : "+component.manufacturer
+            "\n"+lang.trad.manufacturer+" : "+component.manufacturer
         component.details.length>1?
-            str +="\nDetail : " +component.details
+            str +="\n"+lang.trad.detail+" : " +component.details
             :str +=""
-        str += "\nTaille : "+component.component_size+
-            "\nMonture(s) : "+component.mounts+
-            "\nNombre : "+component.quantity+"```"
+        str += "\n"+lang.trad.size+" : "+component.component_size+
+            "\n"+lang.trad.mount+" : "+component.mounts+
+            "\n"+lang.trad.number+" : "+component.quantity+"```"
     }
     return str
 }
@@ -55,95 +55,95 @@ function funcTemplateEmbed(ship){
     templateEmbed.setTimestamp()
     return templateEmbed
 }
-module.exports = async (message,ship) => {
+module.exports = async (message,ship,lang) => {
     let allEmbed = []
     let page1 = funcTemplateEmbed(ship)
     page1.setDescription(ship.description)
-    page1.addField('Taille',"```prolog\n"+capitalize(ship.size)+"```",true)
-    page1.addField('Fabriquant',"```prolog\n"+ship.manufacturerCode+"```",true)
-    page1.addField('Focus',"```prolog\n"+ship.focus+"```",true)
+    page1.addField(lang.trad.size,"```prolog\n"+capitalize(ship.size)+"```",true)
+    page1.addField(lang.trad.manufacturer,"```prolog\n"+ship.manufacturerCode+"```",true)
+    page1.addField(lang.trad.focus,"```prolog\n"+ship.focus+"```",true)
     ship.cargoCapacity?
-        page1.addField('Cargo',"```prolog\n"+ship.cargoCapacity+" SCU```",true)
-        :page1.addField('Cargo',"```prolog\nInconnue```",true)
-    page1.addField('Equipage',"```prolog\n"+ship.maxCrew+"```",true)
-    page1.addField('Type',"```prolog\n"+capitalize(ship.type)+"```",true)
+        page1.addField(lang.trad.cargo,"```prolog\n"+ship.cargoCapacity+" SCU```",true)
+        :page1.addField(lang.trad.cargo,"```prolog\n"+lang.trad.unknown+"```",true)
+    page1.addField(lang.trad.crew,"```prolog\n"+ship.maxCrew+"```",true)
+    page1.addField(lang.trad.type,"```prolog\n"+capitalize(ship.type)+"```",true)
     ship.inGamePrice?
-        page1.addField('Prix Ingame',"```prolog\n"+ship.inGamePrice.replace(',00 €',' AUEC')+"```",true)
-        :page1.addField('Prix Ingame',"```prolog\nInconnue```",true)
+        page1.addField(lang.trad.in_game_price,"```prolog\n"+ship.inGamePrice.replace(',00 €',' AUEC')+"```",true)
+        :page1.addField(lang.trad.in_game_price,"```prolog\n"+lang.trad.unknown+"```",true)
     ship.price?
-        page1.addField('Prix',"```prolog\n"+ship.price.replace(',00 €',' $')+"```",true)
-        :page1.addField('Prix',"```prolog\nInconnue```",true)
-    page1.addField('Statut de production',"```prolog\n"+capitalize(ship.productionStatus)+"```",true)
-    page1.setFooter('Page 1/6')
+        page1.addField(lang.trad.price,"```prolog\n"+ship.price.replace(',00 €',' $')+"```",true)
+        :page1.addField(lang.trad.price,"```prolog\n"+lang.trad.unknown+"```",true)
+    page1.addField(lang.trad.production_status,"```prolog\n"+capitalize(ship.productionStatus)+"```",true)
+    page1.setFooter(lang.trad.page+' 1/6')
     allEmbed.push(page1)
 
     let page2 = funcTemplateEmbed(ship)
-    page2.setTitle(ship.name+' spécification')
-    page2.addField('Longeur',"```prolog\n"+ship.length+" m```",true)
-    page2.addField('Largeur',"```prolog\n"+ship.beam+" m```",true)
-    page2.addField('Hauteur',"```prolog\n"+ship.height+" m```",true)
+    page2.setTitle(ship.name+' '+lang.trad.specification)
+    page2.addField(lang.trad.length,"```prolog\n"+ship.length+" m```",true)
+    page2.addField(lang.trad.width,"```prolog\n"+ship.beam+" m```",true)
+    page2.addField(lang.trad.height,"```prolog\n"+ship.height+" m```",true)
     ship.pitchMax?
-        page2.addField('Tangage Max',"```prolog\n"+ship.pitchMax+" deg/s```",true)
-        :page2.addField('Tangage Max',"```prolog\nInconnue```",true)
+        page2.addField(lang.trad.pitch_max,"```prolog\n"+ship.pitchMax+" deg/s```",true)
+        :page2.addField(lang.trad.pitch_max,"```prolog\n"+lang.trad.unknown+"```",true)
     ship.yawMax?
-        page2.addField('Lacet Max',"```prolog\n"+ship.yawMax+" deg/s```",true)
-        :page2.addField('Lacet Max',"```prolog\nInconnue```",true)
+        page2.addField(lang.trad.yaw_max,"```prolog\n"+ship.yawMax+" deg/s```",true)
+        :page2.addField(lang.trad.yaw_max,"```prolog\n"+lang.trad.unknown+"```",true)
     ship.rollMax?
-        page2.addField('Roulis Max',"```prolog\n"+ship.rollMax+" deg/s```",true)
-        :page2.addField('Roulis Max',"```prolog\nInconnue```",true)
+        page2.addField(lang.trad.roll_max,"```prolog\n"+ship.rollMax+" deg/s```",true)
+        :page2.addField(lang.trad.roll_max,"```prolog\n"+lang.trad.unknown+"```",true)
     ship.xAxisAcceleration?
-        page2.addField("Accélération de l'Axe-X","```prolog\n"+ship.xAxisAcceleration+" m/s²```")
-        :page2.addField("Accélération de l'Axe-X","```prolog\nInconnue```",true)
+        page2.addField(lang.trad.x_axis_acceleration,"```prolog\n"+ship.xAxisAcceleration+" m/s²```")
+        :page2.addField(lang.trad.x_axis_acceleration,"```prolog\n"+lang.trad.unknown+"```",true)
     ship.yAxisAcceleration?
-        page2.addField("Accélération de l'Axe-Y","```prolog\n"+ship.yAxisAcceleration+" m/s²```")
-        :page2.addField("Accélération de l'Axe-Y","```prolog\nInconnue```",true)
+        page2.addField(lang.trad.y_axis_acceleration,"```prolog\n"+ship.yAxisAcceleration+" m/s²```")
+        :page2.addField(lang.trad.y_axis_acceleration,"```prolog\n"+lang.trad.unknown+"```",true)
     ship.zAxisAcceleration?
-        page2.addField("Accélération de l'Axe-Z","```prolog\n"+ship.zAxisAcceleration+" m/s²```")
-        :page2.addField("Accélération de l'Axe-Z","```prolog\nInconnue```",true)
-    page2.setFooter('Page 2/6')
+        page2.addField(lang.trad.z_axis_acceleration,"```prolog\n"+ship.zAxisAcceleration+" m/s²```")
+        :page2.addField(lang.trad.z_axis_acceleration,"```prolog\n"+lang.trad.unknown+"```",true)
+    page2.setFooter(lang.trad.page+' 2/6')
     allEmbed.push(page2)
 
     let page3 = funcTemplateEmbed(ship)
-    page3.setTitle(ship.name+' : Système')
-    page3.addField('Ordinateur',componentStringBuilder(ship.componentJson.RSIAvionic.computers),true)
-    page3.addField('Radar',componentStringBuilder(ship.componentJson.RSIAvionic.radar),true)
-    page3.addField('Alimentation éléctrique',componentStringBuilder(ship.componentJson.RSIModular.power_plants),false)
-    page3.addField('Radiateur',componentStringBuilder(ship.componentJson.RSIModular.coolers),true)
-    page3.addField('Générateur de bouclier',componentStringBuilder(ship.componentJson.RSIModular.shield_generators),true)
-    page3.setFooter('Page 3/6')
+    page3.setTitle(ship.name+' : '+lang.trad.system)
+    page3.addField(lang.trad.computer,componentStringBuilder(ship.componentJson.RSIAvionic.computers),true)
+    page3.addField(lang.trad.radar,componentStringBuilder(ship.componentJson.RSIAvionic.radar),true)
+    page3.addField(lang.trad.power_plant,componentStringBuilder(ship.componentJson.RSIModular.power_plants),false)
+    page3.addField(lang.trad.cooler,componentStringBuilder(ship.componentJson.RSIModular.coolers),true)
+    page3.addField(lang.trad.shield_generators,componentStringBuilder(ship.componentJson.RSIModular.shield_generators),true)
+    page3.setFooter(lang.trad.page+' 3/6')
     allEmbed.push(page3)
 
     let page4 = funcTemplateEmbed(ship)
-    page4.setTitle(ship.name+' : Motorisation')
-    page4.addField('Admission de Carburant', componentStringBuilder(ship.componentJson.RSIPropulsion.fuel_intakes),true)
-    page4.addField('Réservoir de Carburant', componentStringBuilder(ship.componentJson.RSIPropulsion.fuel_tanks),true)
-    page4.addField('Moteur Quantique',componentStringBuilder(ship.componentJson.RSIPropulsion.quantum_drives),true)
-    page4.addField('Réservoir Quantique',componentStringBuilder(ship.componentJson.RSIPropulsion.quantum_drives),true)
-    page4.setFooter('Page 4/6')
+    page4.setTitle(ship.name+' : '+lang.trad.motorization)
+    page4.addField(lang.trad.fuel_intake, componentStringBuilder(ship.componentJson.RSIPropulsion.fuel_intakes),true)
+    page4.addField(lang.trad.fuel_take, componentStringBuilder(ship.componentJson.RSIPropulsion.fuel_tanks),true)
+    page4.addField(lang.trad.quantum_drive,componentStringBuilder(ship.componentJson.RSIPropulsion.quantum_drives),true)
+    page4.addField(lang.trad.quantum_fuel_tanks,componentStringBuilder(ship.componentJson.RSIPropulsion.quantum_fuel_tanks),true)
+    page4.setFooter(lang.trad.page+' 4/6')
     allEmbed.push(page4)
 
     let page5 = funcTemplateEmbed(ship)
-    page5.setTitle(ship.name+' : Proppulsion')
-    page5.addField('Propulseur Principal', componentStringBuilder(ship.componentJson.RSIThruster.main_thrusters),true)
-    page5.addField('Propulseur de Manœuvre', componentStringBuilder(ship.componentJson.RSIThruster.maneuvering_thrusters),true)
-    page5.setFooter('Page 5/6')
+    page5.setTitle(ship.name+' : '+lang.trad.thruster)
+    page5.addField(lang.trad.main_thrusters, componentStringBuilder(ship.componentJson.RSIThruster.main_thrusters),true)
+    page5.addField(lang.trad.maneuvering_thrusters, componentStringBuilder(ship.componentJson.RSIThruster.maneuvering_thrusters),true)
+    page5.setFooter(lang.trad.page+' 5/6')
     allEmbed.push(page5)
 
     let page6 = funcTemplateEmbed(ship)
-    page6.setTitle(ship.name+' : Armement')
+    page6.setTitle(ship.name+' : '+lang.trad.weapon)
     ship.componentJson.RSIWeapon.missiles.length !==0  ?
-        page6.addField('Missile',componentStringBuilder(ship.componentJson.RSIWeapon.missiles))
+        page6.addField(lang.trad.missile,componentStringBuilder(ship.componentJson.RSIWeapon.missiles))
         :null
     ship.componentJson.RSIWeapon.turrets.length !==0 ?
-        page6.addField('Tourelle',componentStringBuilder(ship.componentJson.RSIWeapon.turrets))
+        page6.addField(lang.trad.turrets,componentStringBuilder(ship.componentJson.RSIWeapon.turrets))
         :null
     ship.componentJson.RSIWeapon.weapons.length !==0  ?
-        page6.addField('Armement',componentStringBuilder(ship.componentJson.RSIWeapon.weapons))
+        page6.addField(lang.trad.weapon,componentStringBuilder(ship.componentJson.RSIWeapon.weapons))
         :null
     ship.componentJson.RSIWeapon.utility_items.length !==0  ?
-        page6.addField('Item Utile',componentStringBuilder(ship.componentJson.RSIWeapon.utility_items))
+        page6.addField(lang.trad.utility_items,componentStringBuilder(ship.componentJson.RSIWeapon.utility_items))
         :null
-    page6.setFooter('Page 6/6')
+    page6.setFooter(lang.trad.page+' 6/6')
     allEmbed.push(page6)
 
     new rm.menu(
