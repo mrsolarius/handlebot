3c658317886e9592ced645f7b87dda1308dc5e33
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2020-08-26 08:43:13 +0200

    stats prise en charge de la langue

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -3,9 +3,10 @@ const {countHandle} = require('../interfaces/database/select')
 /**
  *
  * @param {import('discord.js').Message} message
+ * @param  {Lang} lang
  * @return {Promise<void>}
  */
-module.exports = async (message) => {
+module.exports = async (message,lang) => {
     let j = Math.floor((process.uptime() % 31536000) / 86400);
     let h = Math.floor((process.uptime() % 86400) / 3600);
     let m = Math.floor((process.uptime() % 3600) / 60);
@@ -15,13 +16,13 @@ module.exports = async (message) => {
 
     embedComposer.setColor('#007CFF');
     embedComposer.setTitle("Bot Stats");
-    embedComposer.addField("Serveurs", message.client.guilds.cache.size, true);
-    embedComposer.addField("Nombre d'utilisateur", message.client.users.cache.size, true);
-    embedComposer.addField("Nombre d'utiliateur enregistrer",CountUser.toString(10),true)
-    embedComposer.addField("Ping",message.client.ws.ping+" ms",true);
-    embedComposer.addField("RAM Utilisé", (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB", true);
-    embedComposer.addField("Uptime", `${j}j ${h}h ${m}m ${s}s`,true);
-    embedComposer.setFooter(`Démarrer`);
+    embedComposer.addField(lang.trad.servers, message.client.guilds.cache.size, true);
+    embedComposer.addField(lang.trad.user_number, message.client.users.cache.size, true);
+    embedComposer.addField(lang.trad.user_number_in_db,CountUser.toString(10),true)
+    embedComposer.addField(lang.trad.ping,message.client.ws.ping+" ms",true);
+    embedComposer.addField(lang.trad.ram_use, (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB", true);
+    embedComposer.addField(lang.trad.uptime, `${j}j ${h}h ${m}m ${s}s`,true);
+    embedComposer.setFooter(lang.trad.start);
     embedComposer.setTimestamp(client.readyTimestamp)
     await message.channel.send(embedComposer);
 }
