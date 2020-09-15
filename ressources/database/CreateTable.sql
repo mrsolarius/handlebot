84ec96b841b53c384d4cbd0cce676e8f1f5b659d
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2020-09-15 11:20:40 +0200

    Création d'un nouveau shema pour la starMap

--- ressources/database/CreateTable.sql
+++ ressources/database/CreateTable.sql
@@ -1,3 +1,4 @@
+--handle organisation part
 create table lang
 (
     "langISO" char(2),
@@ -57,6 +58,7 @@ create table speak
     constraint fk_lang_speak foreign key ("langID") references lang ("langISO")
 );
 
+--ship part
 create table manufacture
 (
     "manufacturerCode" varchar(10) unique not null,
@@ -103,6 +105,95 @@ create table ship
     constraint fk_ship_manufacturer FOREIGN KEY ("manufacturerCode") references manufacture("manufacturerCode")
 );
 
+--Star Map Schemas
+CREATE TABLE affiliations (
+    "codeAffiliation" VARCHAR(4) NOT NULL,
+    "colorAffiliation" CHAR(7),
+    "name" VARCHAR(45),
+    PRIMARY KEY ("codeAffiliation")
+);
+
+CREATE TABLE stars (
+    "starCode" VARCHAR(45) NOT NULL,
+    "codeAffiliation" VARCHAR(4) NOT NULL,
+    "description" TEXT,
+    "aggregatedDanger" DECIMAL(3,3),
+    "aggregatedEconomy" DECIMAL(3,3),
+    "aggregatedSize" DECIMAL(3,3),
+    "frostLine" REAL,
+    "habitableZoneInner" REAL,
+    "habitableZoneOuter" REAL,
+    "infoUrl" VARCHAR(256),
+    "positionX" REAL,
+    "positionY" REAL,
+    "positionZ" REAL,
+    "imgURL" VARCHAR(256),
+    "type" VARCHAR(45),
+    "status" CHAR(1),
+    constraint pk_stars primary key ("starCode"),
+    constraint fk_affiliation foreign key ("codeAffiliation") references affiliations ("codeAffiliation")
+);
+
+CREATE TABLE types
+(
+    "typeCode" VARCHAR(45) NOT NULL,
+    "nomType" VARCHAR(45),
+    PRIMARY KEY ("typeCode")
+);
+
+CREATE TABLE subType(
+    "subTypeID" INT NOT NULL,
+    "typeCode" VARCHAR(45) NOT NULL,
+    "nomSubType" VARCHAR(45),
+    constraint pk_sub_type primary key ("subTypeID"),
+    constraint fk_parent_type foreign key ("typeCode") references types ("typeCode")
+);
+
+CREATE TABLE starMapObjects
+(
+    "starCode" VARCHAR(45) NOT NULL,
+    "typeCode" VARCHAR(45) NOT NULL,
+    "objCode" VARCHAR(45) NOT NULL,
+    "appearance" VARCHAR(45),
+    "subType" int,
+    "axialTilt" REAL,
+    "name" VARCHAR(45),
+    "description" VARCHAR(45),
+    "designation" VARCHAR(45),
+    "distance" REAL,
+    "fairChanceAct" BOOLEAN,
+    "habitable" BOOLEAN,
+    "info_url" VARCHAR(256),
+    "lat" REAL,
+    "long" REAL,
+    "orbitPeriod" REAL,
+    "sensorDanger" INT,
+    "sensorEconomy" INT,
+    "sensorPopulation" INT,
+    "size" REAL,
+    "imgURL" VARCHAR(256) NULL,
+    constraint fk_star foreign key ("starCode") references stars ("starCode"),
+    constraint fk_type foreign key ("typeCode") references types ("typeCode"),
+    constraint pk_star_map_objects  primary key ("starCode","typeCode","objCode"),
+    constraint fk_sub_type foreign key ("subType") references subType ("subTypeID")
+);
+
+CREATE TABLE jumpPointLinks(
+   "entryStarCode" VARCHAR(45) NOT NULL,
+   "entryTypeCode" VARCHAR(45) NOT NULL,
+   "entryObjCode" VARCHAR(45) NOT NULL,
+   "exitStarCode" VARCHAR(45) NOT NULL,
+   "exitTypeCode" VARCHAR(45) NOT NULL,
+   "exitObjCode" VARCHAR(45) NOT NULL,
+   "name" varchar(12),
+   "direction" char(1),
+   "size" char(1),
+   constraint fk_star_map_object_entry foreign key ("entryStarCode","entryTypeCode","entryObjCode") references starMapObjects ("starCode","typeCode","objCode"),
+   constraint fk_star_map_object_exit foreign key ("exitStarCode","exitTypeCode","exitObjCode") references starMapObjects ("starCode","typeCode","objCode"),
+   constraint pk_jump_point_links primary key ("entryStarCode","entryTypeCode","entryObjCode","exitStarCode","exitTypeCode","exitObjCode")
+);
+
+--Discord stat and utility
 CREATE TABLE Server(
     "guildID" varchar(18),
     "prefix" varchar(3),
